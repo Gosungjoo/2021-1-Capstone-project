420c7323e7cd516e4c981c84682a35f2070667ad
leeyoong <nngeet010@naver.com>
2021-04-12 00:16:09 +0900

    crawling subscribes data

--- Server/capstone/channel/views.py
+++ Server/capstone/channel/views.py
@@ -3,34 +3,81 @@ import json
 from .models import ChannelList
 from django.views import View
 from django.http import HttpResponse, JsonResponse
-
+import time
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver as wd
+from selenium.webdriver.common.keys import Keys
+import chromedriver_autoinstaller
+
 # Create your views here.
 
 
 class ChannelListView(View):
     def post(self,request):
+        driver_path = chromedriver_autoinstaller.install()
         ChannelList.objects.all().delete()
 
         options = wd.ChromeOptions()
         options.add_argument('headless')
         options.add_argument('lang=ko_KR')
         options.add_argument('disable-gpu')
-        options.add_argument(
-            "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
+        options.add_argument('window-size=1920,1080')
+        #options.add_argument(
+        #    "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
 
-        driver = wd.Chrome(executable_path='C:/Program Files (x86)/Google/Chrome/Application/ch', options=options)
+        driver = wd.Chrome(executable_path=driver_path, options=options)
 
         data = json.loads(request.body)
-        url = f'https://www.youtube.com/watch?v={data["channel"]}'
+        url = f'https://www.youtube.com/{data["channel"]}'
         #req = requests.get(
         #    f'https://www.youtube.com/watch?v={data["channel"]}'
         #)
         driver.implicitly_wait(3)
         driver.get(url)
-        print(driver)
+        print(driver.current_url)
+        channel_infos = []
+        imgs = []
+        titles = []
+        subscribers = []
+        cnt = 1
+        try:
+
+            channel_info = driver.find_elements_by_xpath('//*[@id="channel-info"]')
+            for ci in channel_info:
+                print(ci.get_attribute('href'))
+                channel_infos.append(ci.get_attribute('href'))
+                ig = ci.find_element_by_id('img').get_attribute('src')
+                print(ig)
+                imgs.append(ig)
+                tt = ci.find_element_by_id('title').text
+                print(tt)
+                titles.append(tt)
+                sc = ci.find_element_by_id('thumbnail-attribution').text
+                print(sc)
+                subscribers.append(sc)
+                cnt += 1
+                if cnt == 20:
+                    break
+            driver.quit()
+            return JsonResponse({'channel_info': channel_infos, 'img': imgs, 'title': titles,
+                                 'subscribers': subscribers})
+        except:
+            driver.quit()
+            print("No Subscribes data")
+            if len(channel_infos) == 0:
+                return JsonResponse({'channel_info': 'no', 'img': 'no', 'title': 'no',
+                          'subscribers': 'no'})
+            else:
+                return JsonResponse({'channel_info': channel_infos, 'img': imgs, 'title': titles,
+                                     'subscribers': subscribers})
+
+
+
+
+
+
+
         '''
         html = req.text
         #print(html)
