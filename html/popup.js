3ccfce19a848fe59d7ec39d1ac4d607943cd5315
Gosungjoo <wkdsks612@gmail.com>
2021-04-19 01:09:53 +0900

    sox
    mu ya ho!

--- html/popup.js
+++ html/popup.js
@@ -1,92 +1,172 @@
-$(document).ready(function(){
+let myImage = document.querySelector('img');
+let serverdata;
+var scribeindex;
 
 
-  $('#getUrl').click(function(){
 
-      getCurrentTabUrl(function(url) {
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc == 'images/mainlogo(gray).png') {  // gray
+      myImage.setAttribute ('src','images/mainlogo.png'); //red
+      let taburl;
+      getCurrentTabUrl(function(youtubeurl,callback) {
+        alert(youtubeurl.substr(8,));    
+        renderURL(youtubeurl);
+        
+        
+        $.ajax({
+          url: 'http://127.0.0.1:8000/ch/comment',
+          method: 'POST',
+          async : true,
+          data : JSON.stringify({ "comments" : youtubeurl.substr(8,),}),
+          dataType :'json',
+  
+          error: function (request) {
+            alert("Open The Tab watching Youtube");
+        },
+        success: function (res) {
+          serverdata = res.datas;
+          scribeindex = 0;
+          load_subscribe(10);
+        }
+      });
+      
+      
+      });
 
-           renderURL(url);
+        
 
-       });
-       sendURL(url);
+    } else {
+      myImage.setAttribute ('src', 'images/mainlogo(gray).png');
+      scribeindex = -1;
+    }
+}
 
-  })
 
-});
+
+
+
+
+function load_subscribe(c){
+
+    for(var i = 0 ; i < c ; i++ ){
+    if(serverdata.length == scribeindex){
+      scribeindex = -1;
+      alert("no more exist data!");
+    }
+    else{
+    line_distibute(serverdata[scribeindex]);
+    scribeindex++;
+  }
+ 
+  }
+}
+
+
+
+
+function line_distibute(data){
+  //String[]  = data.split(",");
+      addRow(data[0],data[1],data[2],data[3]);
+  
+  }
+
 
 
 
 function getCurrentTabUrl(callback) {
 
-var queryInfo = {
+  var queryInfo = {
 
-  active: true,
+    active: true,
 
-  currentWindow: true
+    currentWindow: true
 
-};
+  };
 
 
-chrome.tabs.query(queryInfo, function(tabs) {
+  chrome.tabs.query(queryInfo, function(tabs) {
 
-  var tab = tabs[0];
+    var tab = tabs[0];
 
-  var url = tab.url;
+    var url = tab.url;
 
-  callback(url);
+    callback(url);
+
+  });
+
+}
+
+
+function renderURL(statusText) {
+
+  /*document.getElementById('urls').textContent = statusText;
+*/
+  alert(statusText);
+
+
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  // when click, get current page link
+
+
+  
+  var link = document.getElementById('getUrl');
+
+
+  link.addEventListener('click', function() {
+    getCurrentTabUrl(function(url) {
+      renderURL(url);
+
+    });
+
+  });
+
 
 });
+document.addEventListener('DOMContentLoaded', function() {
 
-}
+  var link = document.getElementById('subscribe');
+ 
+ link.addEventListener("scroll", function(){
+   var scroll_top = $(this).scrollTop(); //스크롤바의 상단위치
+   var scroll_H = $(this).height(); //스크롤바를 갖는 div의 높이
+   var contentH = $(document.getElementById('tg')).height(); //문서 전체 내용을 갖는 div의 높이
+     $(".top").text(scroll_top);
+      $(".H").text(scroll_H);
+      $(".CH").text(contentH);
+   if(scroll_top + scroll_H +1 >= contentH) { // 스크롤바가 아래 쪽에 위치할 때
+       if(scribeindex != '-1'){
+         load_subscribe(10)
+       }
+       
+   }
+});
+});
 
 
-function renderURL(statusText,) {
+// subscribe table insert
 
-document.getElementById('urls').textContent = statusText;
-
-}
-
-
-function sendURL(youtubeURL){
-  $.ajax({
-      url : '127.0.0.1:8000/ch/list',
-      method : 'POST',
-      data:JSON.stringify({
-          url : URLs
-    })
-  });
-  alert("전송");
-  getData(sendUser);
-  alert("수신");
+function addRow(LinkSource,name,imgUrl,rank) {
+  // table element 찾기
+  const Link = "https://youtube.com/channel/" + LinkSource;
+  const table = document.getElementById('tg');
   
-}
-
-
-
-function getData(){
-$.ajax({
-
-  url : 'https://127.0.0.1:8000/',
-
-  success:function(data){
-
-    alert(data);
-    sendUser(data);
-  }
+  // 새 행(Row) 추가 (테이블 중간에)
+  const newRow = table.insertRow(-1);
   
-})
+  // 새 행(Row)에 Cell 추가
+  const newCell1 = newRow.insertCell(0);
+  const newCell2 = newRow.insertCell(1);
+  //const newCell3 = newRow.insertCell(2);
+  
+  // Cell에 텍스트 추가
+  newCell1.innerHTML = "<a href=" +Link+ " target = blank><img src=" +imgUrl+ " width='50' height='50'>";
+  newCell2.innerHTML = "<a href=" +Link+ " target = blank> &nbsp&nbsp&nbsp<b>" +name+ "</b> ";
+  //newCell3.innerHTML = rank;
 }
-
-function sendUser(user){
-  $.ajax({
-      url : '127.0.0.1:8000/ch/list',
-      method : 'POST',
-      data:JSON.stringify({
-          url : URLs
-    })
-  });
-}
-
-
-chrome.alert("전송");
 
