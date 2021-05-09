20cad5b04156f128aa86fc25c756fc0b332fa242
Gosungjoo <wkdsks612@gmail.com>
2021-05-09 18:40:55 +0900

    gd

--- html/background.js
+++ html/background.js
@@ -1,10 +1,1 @@
-function reddenPage() {
-  document.body.style.backgroundColor = 'red';
-}
-
-chrome.action.onClicked.addListener((tab) => {
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: reddenPage
-  });
-});
+alert("Openzzz The Tab watching Youtube");
